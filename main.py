c5d0ba4699c74d87c5a0835e2829583aa5f8de13
Karthik Ravichandran <karthikravi@Karthiks-MacBook-Pro.local>
2023-11-23 17:43:59 -0500

    Log in created for the app

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
+import yaml
+from yaml.loader import SafeLoader
 import streamlit as st
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
-from  PIL import Image
-import numpy as np
-# import cv2
-import pandas as pd
-# from st_aggrid import AgGrid
-# import plotly.express as px
-import io
-import random
-import pandas as pd
-import streamlit as st
-from dashboard import dashboard
-from alerts import alerts
-from grade_piazza import grade_piazza
+import streamlit_authenticator as stauth
+from menu import menu
 st.set_page_config(layout="wide")
-if 'choose' not in st.session_state:
-    st.session_state['choose'] = None
-with st.sidebar:
-    st.session_state['choose'] = option_menu("Student Event Tracker", ["Dashboard", "Alerts", "Grade/Piazza", "Course Info", "Contact"],
-                             icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
-                             menu_icon="app-indicator", default_index=0,
-                             styles={
-            "container": {"padding": "5!important", "background-color": "#fafafa"},
-            "icon": {"color": "orange", "font-size": "25px"},
-            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "#05ab21"}, ##02ab21
-        }
-        )
 
-if st.session_state['choose'] == "Dashboard":
-    dashboard()
+# hashed_passwords = stauth.Hasher(['karthikcs520', 'aaroncs520', 'deepacs520', 'deepikacs520']).generate()
+# print(hashed_passwords)
 
-elif st.session_state['choose'] == 'Alerts':
-    alerts()
-elif st.session_state['choose'] == 'Grade/Piazza':
-    grade_piazza()
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# credentials – The dictionary of usernames, names, passwords, and emails
+cookies = {"expiry_days":30, "key": "cook", "name": "cs520"}
+# authenticator = stauth.Authenticate({"emails":['jsmith@gmail.com', 'rbriggs@gmail.com'],
+#                                      "names": ['John Smith', 'Rebecca Briggs'],
+#                                      "passwords": ['abc', 'def'],
+#                                      "usernames": ['jsmith', 'rbriggs']}, key ='something',
+#                                     cookie_name='cs520')
+
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    menu()
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+# if st.session_state["authentication_status"]:
+#     try:
+#         if authenticator.reset_password(st.session_state["username"], 'Reset password'):
+#             st.success('Password modified successfully')
+#     except Exception as e:
+#         st.error(e)
+#
+# try:
+#     if authenticator.register_user('Register user', preauthorization=False):
+#         st.success('User registered successfully')
+# except Exception as e:
+#     st.error(e)
